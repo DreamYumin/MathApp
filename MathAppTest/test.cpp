2b5ecd7a881fa5d3abc8b9e0c3bf4e82ddc74e37
DreamYumin <DreamYuminmy@gmail.com>
2020-05-28 22:08:47 -0400

    Test: 增加加法负运算的支持
    新增测试，将之前正运算的变负放入新测试。

--- MathAppTest/test.cpp
+++ MathAppTest/test.cpp
@@ -2,4 +2,12 @@
 #include "../MathApp/doubleNum.cpp"
 TEST(DoubleNumTest, positiveValues) {
 	ASSERT_EQ(12, doubleNum(6));
+	ASSERT_EQ(4, doubleNum(2));
+	ASSERT_EQ(40, doubleNum(20));
+}
+
+TEST(DoubleNumTest, negativeValues) {
+	ASSERT_EQ(-12, doubleNum(-6));
+	ASSERT_EQ(-4, doubleNum(-2));
+	ASSERT_EQ(-40, doubleNum(-20));
 }
